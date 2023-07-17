4d925dad5ec05ea70fee6283d9b7794ec5825983
Aaron Bieber <aaron@bolddaemon.com>
2023-07-17 09:26:14 -0600

    update deps, bump to 0.2.5

--- flake.nix
+++ flake.nix
@@ -3,54 +3,53 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
-      overlay = final: prev: {
-        gostart = self.packages.${prev.system}.gostart;
-      };
-      nixosModule = import ./module.nix;
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          gostart = pkgs.buildGo120Module {
-            pname = "gostart";
-            version = "v0.2.4";
-            src = ./.;
-
-            vendorHash =
-              "sha256-V8daJcDitpJIoEd9eURIyRPlpx4hoFvfsDI5c2GzWg0=";
-          };
-        });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.gostart);
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            shellHook = ''
-              PS1='\u@\h:\@; '
-              nix run github:qbit/xin#flake-warn
-              echo "Go `${pkgs.go}/bin/go version`"
-            '';
-            nativeBuildInputs = with pkgs; [
-              elmPackages.elm
-              entr
-              git
-              go-tools
-              go_1_20
-              gopls
-              rlwrap
-              sqlc
-              sqlite
-              nodePackages.uglify-js
-            ];
-          };
-        });
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    overlay = final: prev: {
+      gostart = self.packages.${prev.system}.gostart;
     };
-}
+    nixosModule = import ./module.nix;
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      gostart = pkgs.buildGo120Module {
+        pname = "gostart";
+        version = "v0.2.5";
+        src = ./.;
 
+        vendorHash = "sha256-guREvEipql96RgCAejIEl5DZSynYOPHttzjrlxVOUX4=";
+      };
+    });
+
+    defaultPackage = forAllSystems (system: self.packages.${system}.gostart);
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        shellHook = ''
+          PS1='\u@\h:\@; '
+          nix run github:qbit/xin#flake-warn
+          echo "Go `${pkgs.go}/bin/go version`"
+        '';
+        nativeBuildInputs = with pkgs; [
+          elmPackages.elm
+          entr
+          git
+          go-tools
+          go_1_20
+          gopls
+          rlwrap
+          sqlc
+          sqlite
+          nodePackages.uglify-js
+        ];
+      };
+    });
+  };
+}
